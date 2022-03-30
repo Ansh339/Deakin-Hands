9ca13aeb06417b5ef93e2fb4cf06d61ed47bdc6b
Ansh339 <90087682+Ansh339@users.noreply.github.com>
2022-03-30 14:26:02 +0530

    Update server.js
    Web pages that are being added are now getting linked to the server.

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express();
 
 const port = 3000;
 const base = `${__dirname}/public`;
+
+app.use(express.static('public'));
 
 /* TODO:
 The login page seems to work well enough but the formatting does not load in properly through the server. Fix this in the near future
 */
 app.get('/', function (req, res) {
     res.sendFile(`${base}/login_page.html`);
+});
+
+app.get('/register-device', (req, res) => {
+    res.sendFile(`${base}/register_student.html`);
 });
 
 app.get('*', (req, res) => {
